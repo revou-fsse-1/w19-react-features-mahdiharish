e19eb35f70d485c9ffabab569b946bdca27a4f4f
mahdiharish <mahdiharish12@gmail.com>
2023-06-17 01:56:39 +0800

    add dynamic category details page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard/Dashboard';
 import NotFound from './components/NotFound';
-import PrivateRoute from './components/PrivateRoute';
+import CategoryDetails from './components/Dashboard/CategoryList';
+
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  const isAuthenticated = true;
+
+  return isAuthenticated ? <>{children}</> : null;
+}
 
 function App() {
   return (
@@ -12,8 +18,8 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/category/:categoryId" element={<PrivateRoute><CategoryDetails /></PrivateRoute>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
