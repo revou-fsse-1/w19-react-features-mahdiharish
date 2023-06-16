d9edb200128da31152148032d66494f45216af51
mahdiharish <mahdiharish12@gmail.com>
2023-06-16 14:06:45 +0800

    add axios and material UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import NotFound from './components/NotFound';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -13,7 +13,8 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
