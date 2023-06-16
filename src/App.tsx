598ea5aaf6ef114e4fb3f6be9c497f3491765ed2
mahdiharish <mahdiharish12@gmail.com>
2023-06-16 18:02:05 +0800

    fix register function

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} /> */}
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
   email: yup.string().required('Email is required').email('Invalid email address'),
   password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters long'),
 });
@@ -15,7 +16,7 @@ function Register() {
 
   const navigate = useNavigate();
 
-  const handleRegister = async (data: { email: string; password: string }) => {
+  const handleRegister = async (data: { name: string, email: string; password: string }) => {
     try {
       console.log(data)
       const response = await fetch('https://mock-api.arikmpt.com/api/user/register', {
@@ -43,6 +44,11 @@ function Register() {
       <div className="bg-white p-8 rounded shadow-lg">
         <h2 className="text-2xl font-bold mb-4">Register</h2>
         <form onSubmit={handleSubmit(handleRegister)}>
+            <div className="mb-4">
+              <label className="block mb-2">Name:</label>
+              <input type="text" {...register('name')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+              {errors.name && <p>{errors.name.message}</p>}
+          </div>
           <div className="mb-4">
             <label className="block mb-2">Email:</label>
             <input type="text" {...register('email')} className="border border-gray-300 px-3 py-2 rounded w-full" />
