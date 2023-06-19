24549a274865c505111b7c3af12f0e74aa387929
mahdiharish <mahdiharish12@gmail.com>
2023-06-19 15:58:01 +0800

    add useMemo

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -93,30 +93,38 @@ function Dashboard() {
     navigate(`/category/edit/${categoryId}`);
   }, [navigate]);
 
+  const activeCategories = useMemo(() => {
+    return categories.filter(category => category.is_active);
+  }, [categories]);
+
+  const inactiveCategories = useMemo(() => {
+    return categories.filter(category => !category.is_active);
+  }, [categories]);
+
   return (
     <div className="container mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold my-6">Dashboard Page</h1>
       <div className="my-6">
         <h2 className="text-xl font-semibold mb-4">Add New Category</h2>
-        <form onSubmit={handleSubmit(handleAddCategory)} className="w-full md:w-64">
-          <div className="mb-4">
-            <label htmlFor="name" className="block mb-2">Name:</label>
-            <input type="text" id="name" {...register('name')} className="border border-gray-300 rounded px-2 py-1 w-full" />
+        <form onSubmit={handleSubmit(handleAddCategory)} className="flex items-end">
+          <div className="mr-2">
+            <input type="text" {...register('name')} placeholder="Category Name" className="border p-2" />
             {errors.name && <p className="text-red-500">{errors.name.message}</p>}
           </div>
-          <div className="mb-4">
-            <label htmlFor="is_active" className="block mb-2">Status:</label>
-            <select id="is_active" {...register('is_active')} className="border border-gray-300 rounded px-2 py-1 w-full">
-              <option value="">Select status</option>
+          <div className="mr-2">
+            <select {...register('is_active')} className="border p-2">
+              <option value="">Select Status</option>
               <option value="true">Active</option>
               <option value="false">Inactive</option>
             </select>
             {errors.is_active && <p className="text-red-500">{errors.is_active.message}</p>}
           </div>
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 w-full">Add Category</button>
-          {successMessage && <p className="text-green-500 my-2">{successMessage}</p>}
-          {errorMessage && <p className="text-red-500 my-2">{errorMessage}</p>}
+          <button type="submit" className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-700">
+            Add Category
+          </button>
         </form>
+        {successMessage && <p className="text-green-500">{successMessage}</p>}
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       </div>
       <div className="my-6">
         <h2 className="text-xl font-semibold mb-4">Categories</h2>
@@ -130,14 +138,36 @@ function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            {categories.map((category) => (
+            {activeCategories.map((category) => (
               <tr key={category.id ?? ''}>
                 <td className="border-b py-2 px-2 md:px-4">{category.id}</td>
                 <td className="border-b py-2 px-2 md:px-4">{category.name}</td>
                 <td className="border-b py-2 px-2 md:px-4">{category.is_active ? 'Active' : 'Inactive'}</td>
                 <td className="border-b py-2 px-2 md:px-4">
-                  <button onClick={() => handleEditCategory(category.id!)} className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-700 mr-2">Edit</button>
-                  <button onClick={() => handleDeleteCategory(category.id!)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 mr-2">Delete</button>
+                  <button onClick={() => handleEditCategory(category.id!)} className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-700 mr-2">
+                    Edit
+                  </button>
+                  <button onClick={() => handleDeleteCategory(category.id!)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 mr-2">
+                    Delete
+                  </button>
+                  <Link to={`/category/${category.id}`} className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-700 mr-2">
+                    Details
+                  </Link>
+                </td>
+              </tr>
+            ))}
+            {inactiveCategories.map((category) => (
+              <tr key={category.id ?? ''}>
+                <td className="border-b py-2 px-2 md:px-4">{category.id}</td>
+                <td className="border-b py-2 px-2 md:px-4">{category.name}</td>
+                <td className="border-b py-2 px-2 md:px-4">{category.is_active ? 'Active' : 'Inactive'}</td>
+                <td className="border-b py-2 px-2 md:px-4">
+                  <button onClick={() => handleEditCategory(category.id!)} className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-700 mr-2">
+                    Edit
+                  </button>
+                  <button onClick={() => handleDeleteCategory(category.id!)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 mr-2">
+                    Delete
+                  </button>
                   <Link to={`/category/${category.id}`} className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-700 mr-2">
                     Details
                   </Link>
